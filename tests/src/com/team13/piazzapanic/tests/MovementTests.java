4cfebf2ee70645c680a1ae3563291638cb863e3f
chrispen656 <c.d.hartley@outlook.com>
2023-05-02 02:11:58 +0100

    Implemented basic framework for testing and a simple chef movement and collision test

---
+++ tests/src/com/team13/piazzapanic/tests/MovementTests.java
@@ -0,0 +1,37 @@
+package com.team13.piazzapanic.tests;
+
+import Sprites.Chef;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
+
+
+@RunWith(GdxTestRunner.class)
+public class MovementTests {
+
+    @Test
+    public void testChefMove() {
+        World world = new World(new Vector2(0, 0), true);
+        Chef chef = new Chef(world, 31.5F, 65, false);
+
+        //Change velocity of chef to move
+        float initialY = chef.getY();
+        chef.b2body.setLinearVelocity(0f, -0.5f);
+        chef.update(0.0001f);
+        //Check pos of chef if lower down
+        assertTrue(initialY < chef.getY());
+    }
+
+    @Test
+    public void testChefCollision() {
+        World world = new World(new Vector2(0, 0), true);
+        Chef chef1 = new Chef(world, 31.5F, 65, false);
+        Chef chef2 = new Chef(world, 31.5F, 65, false);
+        chef1.chefsColliding();
+        chef2.chefsColliding();
+        //Check game knows chefs are colliding when the function is called
+        assertTrue(chef1.chefOnChefCollision && chef2.chefOnChefCollision);
+    }
+}
